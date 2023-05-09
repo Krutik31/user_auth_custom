9d42445cab4febcbb3f4ff198f238efe93eac4bf
Krutik Patel <krutik.p@simformsolutions.com>
2023-05-09 18:37:40 +0530

    feat: TE7-T129 User advance login and authentication in rails
    Approach 1: Custom Implementation
    
    	Set up a new Rails 7 application.
    	Create a User model with the following attributes: username, email, password_digest, and remember_token.
    	Implement the has_secure_password and has_secure_token features in the User model to handle password authentication and remember me functionality.
    	Create a UsersController to handle user registration, login, logout, and session management.
    	Use Rails forms to generate views for user registration, login, and logout.
    	Implement form validation using client-side form validation provided by Rails.
    	Use Rails flash messages to display temporary messages after successful login/logout or failed login attempts.
    	Write integration tests to verify correct routes, database updates, and proper changes to the layout.
    	Implement user authentication and login status to restrict access to certain pages and features.

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,23 @@
+class SessionsController < ApplicationController
+  skip_before_action :authenticate_user, only: %i[new create]
+  before_action :redirect_if_authenticated, only: %i[new create]
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.find_by(username: params[:user][:username])
+    if @user.present? && @user.authenticate(params[:user][:password])
+      session[:user_id] = @user.id
+      redirect_to root_path, flash: { success: 'Logged in successfully' }
+    else
+      redirect_to new_session_path, flash: { danger: 'Username & Password has not matched.' }
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    redirect_to root_path, flash: { danger: 'Logged Out' }
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,11 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root 'users#index'
+
+  resources :registrations, only: %i[new create]
+
+  resources :users, only: %i[index]
+
+  resources :sessions, only: %i[new create destroy]
 end
